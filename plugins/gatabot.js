28fe22ebcd6ceb5500e6964147cbc059e21e7886
dhrade <dhrade@gmail.com>
2022-06-11 23:37:46 -0500

    Update gatabot.js

--- plugins/gatabot.js
+++ plugins/gatabot.js
@@ -9,7 +9,7 @@ const estiloaudio = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ..
 message: { 
 "audioMessage": { "mimetype":"audio/ogg; codecs=opus", "seconds": "99569", "ptt": "true"   
 }}}  
-conn.sendButton(m.chat, `*¡Hola! Visita el menú 🐈*`, 'Gata Bot', '𝙈𝙀𝙉𝙐 𝙉𝙐𝙀𝙑𝙊', `${usedPrefix}menu`, 'conversation', { sendEphemeral: true, quoted: estilo })
+conn.sendButton(m.chat, `*¡Hola! Visita el menú 🤡*`, 'CrazyBot', '𝙈𝙀𝙉𝙐 𝙉𝙐𝙀𝙑𝙊', `${usedPrefix}menu`, 'conversation', { sendEphemeral: true, quoted: estilo })
 await conn.sendFile(m.chat, vn, 'pikachu.mp3', null, m, true, { type: 'audioMessage', ptt: true, sendEphemeral: true, quoted: estiloaudio })
 }
 handler.command = /^(ot|ata)$/i
